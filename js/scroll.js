7e81dbc7c6dd43b5b4b23903ceef00f44717e3fe
jobayerimtiaz <jiefq.37@gmail.com>
2024-09-24 16:56:28 +0600

    information added in blog section

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,11 @@
 
     window.addEventListener("scroll", function() {
         const header = document.getElementById("header");
-        if (window.scrollY > 50) {  // When scrolled more than 50px
-            header.classList.add("backdrop-blur-md", "bg-opacity-80");  // Add blur and opacity change
+        if (window.scrollY > 50) {  
+            header.classList.add("backdrop-blur-md", "bg-opacity-80"); 
+             
         } else {
-            header.classList.remove("backdrop-blur-md", "bg-opacity-80");  // Remove blur if scrolled back to the top
+            header.classList.remove("backdrop-blur-md", "bg-opacity-80"); 
         }
     });
 
